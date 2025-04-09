42780b1394f7927af81f1ae0433415334c51ab2c
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-09 17:25:09 +0000

    feat: add API key generation and hashing during user registration

--- src/controllers/auth/register.controller.js
+++ src/controllers/auth/register.controller.js
@@ -5,6 +5,7 @@ import { generateUsername } from "../../utils/generateUsername.js";
 import { registerUserSchema } from '../../validations/registerUser.schema.js'
 import { User } from "../../models/user.model.js"
 import bcrypt from "bcryptjs";
+import crypto from 'crypto';
 
 export const registerUser = AsyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
@@ -23,16 +24,21 @@ export const registerUser = AsyncHandler(async (req, res) => {
     };
     // hash user password 
     const hashedPassword = await bcrypt.hash(password, 12);
+    // generate api key 
+    const rowApiKey = crypto.randomBytes(32).toString("hex");
+    // hash api key for security 
+    const hashApiKey = crypto.createHash("sha256").update(rowApiKey).digest("hex")
     // save new user in db 
     const newUser = await User.create({
         name,
         email,
         username: generateUsername(name),
-        password: hashedPassword
+        password: hashedPassword,
+        apiKey: hashApiKey
     });
     // remove sensitive information
     const user = await User.findById(newUser._id).select("-password -apiKey -refreshToken -updatedAt");
 
     // return response 
-    return res.status(201).json(new ApiResponse(201, "user register successful", user));
+    return res.status(201).json(new ApiResponse(201, "user register successful", {user,apiKey:rowApiKey}));
 });
