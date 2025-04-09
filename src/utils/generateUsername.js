853b25dd338e626223bfbb2b0b4f163aab61d5e7
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-09 17:13:08 +0000

    fix: ensure username prefix is lowercase and split by space

--- src/utils/generateUsername.js
+++ src/utils/generateUsername.js
@@ -2,5 +2,5 @@ import crypto from 'crypto';
 
 export const generateUsername = (prefix = "user", byte = 6) => {
     const randomString = crypto.randomBytes(byte).toString("hex");
-    return `${prefix}_${randomString}`
+    return `${prefix.split(" ")[0].toLowerCase()}_${randomString}`
 }
