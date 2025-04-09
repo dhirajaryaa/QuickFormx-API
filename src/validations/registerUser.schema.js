e63bebd8c91df369e2eccffd0a07f071600ae10e
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-09 16:09:39 +0000

    refactor: add name validation to registerUser schema

--- src/validations/registerUser.schema.js
+++ src/validations/registerUser.schema.js
@@ -2,6 +2,10 @@ import { z } from "zod";
 import { passwordSchema } from "./password.schema.js"
 
 export const registerUserSchema = z.object({
+    name: z
+        .string()
+        .min(2, "Name must be at least 2 characters long")
+        .max(50, "Name can't be more than 50 characters"),
     email: z.string()
         .email()
         .regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid Email"),
