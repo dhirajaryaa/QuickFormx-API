be39337490460d3b62e968cd745253bb11a4f226
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-09 17:13:15 +0000

    fix: correct maximum password length validation message

--- src/validations/password.schema.js
+++ src/validations/password.schema.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const passwordSchema = z
     .string()
     .min(6, "Password must be at least 6 characters long")
-    .max(6, "Password can't be more than 64 characters")
+    .max(64, "Password can't be more than 64 characters")
     .regex(
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).*$/,
         "Password must include uppercase, lowercase, number, and special character"
