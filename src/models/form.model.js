11fd55395839a2126b159e32076cab5296519d48
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-10 14:07:53 +0000

    feat: add form model with field schema for dynamic form creation

---
+++ src/models/form.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const fieldSchema = new Schema({
+    label: String,
+    name: String,
+    required: {
+        type: Boolean,
+        default: false
+    },
+    type: {
+        type: String,
+        enum: ["text", "textarea", "email", "number", "radio", "checkbox", "select", "date", "file"],
+        required: true
+    },
+    placeholder: { type: String },
+    options: [String],  // for select , checkbox , radio button
+}, { _id: false });
+
+const formSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    fields: [fieldSchema],
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: User
+    }
+}, { timestamps: true });
+
+export const Form = mongoose.model("Form",formSchema);
