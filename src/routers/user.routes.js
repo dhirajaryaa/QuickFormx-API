c316dbc2df7230b6c0a341c0b3399da7569a80e9
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-10 01:44:49 +0000

    feat: implement user profile update functionality with validation

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { authorizedUser } from "../middlewares/auth.middleware.js";
 import { getUserProfile } from "../controllers/user/getProfile.controller.js";
+import { updateUserProfile } from "../controllers/user/updateProfile.controller.js";
 
 export const router = Router();
 
 //! protected Routes
 // get user profile
 router.get("/profile", authorizedUser, getUserProfile );
+// update user profile
+router.put("/profile", authorizedUser, updateUserProfile );
