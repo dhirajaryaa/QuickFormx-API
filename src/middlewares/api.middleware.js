4376317ec783a1ad28b15aa8d2de4b2859d06031
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-11 09:29:01 +0000

    refactor: improve API key validation error handling in ApiKeyChecker middleware

--- src/middlewares/api.middleware.js
+++ src/middlewares/api.middleware.js
@@ -5,9 +5,11 @@ import { User } from "../models/user.model.js";
 import { apiKeySchema } from "../validations/form/apiKey.schema.js"
 
 export const ApiKeyChecker = AsyncHandler(async (req, res, next) => {
-    const incomingApiKey = apiKeySchema.safeParse(req.headers["x-api-key"]);
-    if (!incomingApiKey.success) {
-        return res.status(400).json({ message: incomingApiKey.error.issues[0].message });
+    const incomingApiKey = req.headers["x-api-key"];
+    // check api valid format 
+    const apiCheck = apiKeySchema.safeParse(incomingApiKey);
+    if (!apiCheck.success) {
+        throw new ApiError(400, apiCheck.error.issues[0].message);
     };
     // hash api key 
     const hashApiKey = crypto.createHash("sha256").update(incomingApiKey).digest("hex");
