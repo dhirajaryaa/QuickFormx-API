8c41a3715d8709ae47a97206832a236bc0bcbabc
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-11 00:27:43 +0000

    feat: implement getSingleForm controller and validation schema for form retrieval

--- src/routers/form.routes.js
+++ src/routers/form.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authorizedUser } from "../middlewares/auth.middleware.js";
 import { createNewForm } from "../controllers/form/create.controller.js";
+import { getSingleForm } from "../controllers/form/getForm.controller.js";
 
 export const router = Router();
 
 //! protected Routes
 // create new form
 router.post("/", authorizedUser, createNewForm);
+// get form using id
+router.get("/:id", authorizedUser, getSingleForm);
 
