3fc4e09db24de442c9ababa5fd90fa42f116c0ec
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-04-11 12:04:10 +0000

    feat: create Submission model with schema for form submissions

---
+++ src/models/submission.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const submissionSchema = new Schema({
+    formId: {
+        type: Schema.Types.ObjectId,
+        ref: "Form",
+        required: [true, "Form ID is required"]
+    },
+    data: {
+        type: String,
+        required: [true, "Submission data is required"]
+    },
+    submitAt: {
+        type: Date,
+        default: Date.now
+    },
+    clientIp: {
+        type: String,
+    }
+});
+
+export const Submission = mongoose.model("Submission", submissionSchema);
